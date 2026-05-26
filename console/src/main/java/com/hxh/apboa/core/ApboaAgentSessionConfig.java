dc0e9906aca75d8be13cacc4b9a4270c544d6cf6
liuwei <wei.liu@52ydwf.com>
2026-05-26 11:57:41 +0800

    fix: 优化agentSession数据库名称获取方式，遵循JDBC规范

--- console/src/main/java/com/hxh/apboa/core/ApboaAgentSessionConfig.java
+++ console/src/main/java/com/hxh/apboa/core/ApboaAgentSessionConfig.java
@@ -11,13 +11,11 @@ import io.agentscope.spring.boot.agui.mvc.AguiMvcController;
 import io.agentscope.spring.boot.agui.webflux.AguiWebFluxHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
